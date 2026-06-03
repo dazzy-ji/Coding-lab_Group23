de01bfa5ba2e6dbde851ad505dab67528f21068f
jlouange <j.mushumba@alustudent.com>
2026-06-03 08:21:46 -0700

    Added comments

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-initialize_system
-secure_data
+initialize_system #calling the function called initialize_system
+secure_data #calling a function called secure_data
 
 echo "Sytem Environment Secured"
 
-date +%Y-%m-%d
+date +%Y-%m-%d #this is the variable that shows date by cutting out minutes and seconds
