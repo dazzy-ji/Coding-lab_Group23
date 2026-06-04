385400311441a4acb8f4fd0c429f854e21d45078
Bruno-SHEMA <b.shema3@alustudent.com>
2026-06-04 06:12:46 -0700

    hospital data archiving operations

--- hospital_analysis.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-process_vitals(){
-grep "CRITICAL" active_logs/heart_rate_log.log | awk -F '|' '{print $1, $2, $3}' >> reports/critical_alerts.txt
-grep "CRITICAL" active_logs/temperature_log.log | awk -F '|' '{print $1, $2, $3}' >> reports/critical_alerts.txt
-}
-process_vitals
